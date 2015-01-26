30a365c9f483a2641ad427267f589019f4857134
allensmilko <jeisoncastaneda@emotion-cd.com>
2015-01-26 00:25:49 -0500

    okok2
    okok2

--- js/ingreso.js
+++ js/ingreso.js
@@ -93,7 +93,7 @@ $(document).ready(function(){
             '</div>'+
             //'<button class="removeclass">-</button>'+
             
-             '<input type="image" class="removeclass agrequitt" src="img/menos.png" />'+
+             '<input type="button" class="removeclass agrequitt" src="img/menos.png" />'+
              '</div>'
                      );
     }
